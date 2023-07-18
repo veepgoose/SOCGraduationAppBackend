b8dc61c222d474ca8a2c6028fd4570dec05bf5a7
Tom Webb <tomwebb1992@gmail.com>
2023-07-18 11:37:38 +0100

    implimented backend maybe

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import express from "express";
-import attendeesRoutes from './routes/attendeesRoutes.js';
+import express from 'express';
+import pkg from 'pg';
+import cors from 'cors';
+import pool from './database/dbLink.js';
 
 const app = express();
-const port = 3001;
 app.use(express.json());
-app.use("/attendees", attendeesRoutes);
+app.use(cors());
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+// Route to handle form data submission
+app.post('/submit-form', async (req, res) => {
+  try {
+    const formData = req.body;
+
+    const insertQuery = `
+      INSERT INTO your_table_name (column1, column2, column3)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `;
+
+    const values = [formData.value1, formData.value2, formData.value3]; // Replace with the actual values you want to insert
+
+    const { rows } = await pool.query(insertQuery, values);
+    console.log('Inserted data:', rows[0]);
+
+    return res.status(201).json({ message: 'Form data saved successfully!' });
+  } catch (error) {
+    console.error('Error handling form data submission:', error);
+    return res.status(500).json({ error: 'An error occurred while processing the form data.' });
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Backend server is running on port ${PORT}`);
 });
--- database/dbLink.js
+++ database/dbLink.js
@@ -1,8 +1,28 @@
 import pkg from 'pg';
-const {Pool} = pkg;
+const { Pool } = pkg;
+
+
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,});
-
-
+  connectionString: 'postgres://yipglpsr:wX6Y9OLeAy__pUrJQwErK4idLd1dI0eA@horton.db.elephantsql.com/yipglpsr',
+});
 
 export default pool;
+
+
+
+
+
+
+
+//ARCHIVED CODE
+// import { Pool } from 'pg';
+
+// const pool = new Pool({
+//   connectionString: ,
+// });
+
+// const query = (text, params, callback) => {
+//   return pool.query(text, params, callback);
+// };
+
+// export { query };
